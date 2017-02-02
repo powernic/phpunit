faa8d1a1bf25132cbc79af8cc975253c7c826e46
Nikolay Efimenko <powernic@list.ru>
2017-02-02 03:59:25 +0300

    Инициализация Travis

---
+++ tests/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Powernic
+ * Date: 02.02.2017
+ * Time: 3:55
+ */
+class FirstTest extends PHPUnit_Framework_TestCase
+{
+    public function testFirst(){
+        $this->assertEquals(1,1);
+    }
+}
